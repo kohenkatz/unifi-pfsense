b73bdca4993de582834e7ade737411398197c370
MK <kohenkatz@gmail.com>
2014-11-23 15:42:25 -0500

    use FreeBSD 10 pkg command

--- install-unifi/install-unifi.sh
+++ install-unifi/install-unifi.sh
@@ -3,12 +3,6 @@
 # install-unifi.sh
 # Installs the Uni-Fi controller software on a FreeBSD machine (presumably running pfSense).
 
-# Set the package source to the ftp package archive, since pfsense 2.1.x is
-# still using the EOL'd FreeBSD 8.3, and those packages are no longer
-# available in the primary repo:
-
-export PACKAGESITE=ftp://ftp-archive.freebsd.org/pub/FreeBSD-Archive/ports/`/usr/bin/uname -m`/packages-8.3-release/Latest/
-
 # Stop the controller if it's already running...
 # First let's try the rc script if it exists:
 if [ -f /usr/local/etc/rc.d/unifi ]; then
@@ -59,7 +53,7 @@ echo " done."
 # Install mongodb, OpenJDK 7, and unzip (required to unpack Ubiquiti's download):
 # -F skips a package if it's already installed, without throwing an error.
 echo -n "Installing required packages..."
-/usr/sbin/pkg_add -vFr mongodb openjdk7 unzip
+/usr/sbin/pkg install mongodb openjdk7 unzip
 echo " done."
 
 # Switch to a temp directory for the Unifi download:
